59176cc9983288144ebbc4dee0cb25e0bac6f7e5
richard70689 <45480571+richard70689@users.noreply.github.com>
2018-12-01 13:53:29 +0800

    Add files via upload

---
+++ CreditCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Credit_Card_Validation.Models
+{
+    public class CreditCard
+    {
+        public int fldSeqNo { get; set; }
+        public string fldCardNumber { get; set; }
+        public string fldExpiryDate { get; set; }
+        public string fldCardType { get; set; }
+    }
+}
---
+++ CustomerCreditCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Mvc;
+
+namespace Credit_Card_Validation.Models
+{
+    public class CustomerCreditCard
+    {
+        private CreditCardDBEntities1 db = new CreditCardDBEntities1();
+        public IQueryable<tblCreditCardsList> findAll()
+        {
+            return db.tblCreditCardsLists;
+        }
+
+        public IEnumerable<sp_CreditCardNumberValidator_Result> GetCreditCardRecord(string cardNo)
+        {
+            return db.sp_CreditCardNumberValidator(cardNo).ToArray();
+        }
+       
+    }
+}
