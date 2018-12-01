17709ea54296c6a5bb5438d93aca414e9a2edcf9
richard70689 <45480571+richard70689@users.noreply.github.com>
2018-12-01 13:52:55 +0800

    Add files via upload

---
+++ CreditCardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Credit_Card_Validation.Models;
+
+namespace Credit_Card_Validation.Controllers
+{
+    public class CreditCardController : Controller
+    {
+        
+        // GET: CreditCard
+        public ActionResult Index()
+        {
+            CustomerCreditCard cc = new CustomerCreditCard();
+            ViewBag.listCreditCards = cc.findAll();
+            return View();
+        }
+
+        [HttpPost]       
+        public ActionResult Index(FormCollection form)
+        {
+            CustomerCreditCard cc = new CustomerCreditCard();
+            IEnumerable <sp_CreditCardNumberValidator_Result> ct = cc.GetCreditCardRecord(form["SearchString"].ToString());
+            ViewBag.listCreditCards = cc.GetCreditCardRecord(form["SearchString"].ToString());
+            ViewBag.CardType = ct.First().fldCardType;
+            return View();
+        }
+    }
+}
